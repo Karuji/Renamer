10ef07fea021b8e14affc9eac1bf4e8918cd987f
Karuji <Code@Karuji.net>
2014-01-23 15:54:27 +0200

    Split FileName.py from Renamer.py as FileName.py is now being used in multiple projects.

---
+++ FileName.py
@@ -0,0 +1,50 @@
+import os
+
+class FileName(object):
+    """Class to hold the name of the file to be renamed."""
+    string    = "" #not to be changed
+    file      = ""
+    extention = ""
+    newName   = ""
+    hasExt    = False
+
+    def __init__(self, string):
+        """Allows passing the current file name when creating the object."""
+        self.string = string
+        pos = 0
+
+        if '.' in self.string:
+            for i in range(1, len(self.string)):
+                if self.string[-i] == ".":
+                    pos = i
+                    break
+            self.file = self.string[:len(self.string)-pos]
+            self.extention = self.string[-pos:]
+        else:
+            self.file = self.string
+
+    def setFileName(self, name):
+        """Sets a new file name for the rename function."""
+        self.file = name
+
+    def getName(self): 
+        """Returns the current full name of the file."""
+        return self.string
+
+    def setName(self, string):
+        """Sets the member string var."""
+        self.string = string
+
+    def getFileName(self): 
+        """Returns the current file name that it shall be renamed to."""
+        return self.file
+
+    def getFileExt(self):
+        """Returns the current file extention."""
+        return self.extention
+
+    def rename(self):
+        """renames the file from the current name (string) to the new one (file)."""
+        self.newName = self.file + self.extention
+        os.rename(self.string, self.newName)
+        print(self.string + "-->" + self.newName)
--- Renamer.py
+++ Renamer.py
@@ -2,55 +2,56 @@
 """Program for the bulk renaming of files in an ordered manner."""
 
 import os
+from FileName import *
 
-class FileName(object):
-    """Class to hold the name of the file to be renamed."""
-    string    = "" #not to be changed
-    file      = ""
-    extention = ""
-    newName   = ""
-    hasExt    = False
+# class FileName(object):
+#     """Class to hold the name of the file to be renamed."""
+#     string    = "" #not to be changed
+#     file      = ""
+#     extention = ""
+#     newName   = ""
+#     hasExt    = False
 
-    def __init__(self, string):
-        """Allows passing the current file name when creating the object."""
-        self.string = string
-        pos = 0
+#     def __init__(self, string):
+#         """Allows passing the current file name when creating the object."""
+#         self.string = string
+#         pos = 0
 
-        if '.' in self.string:
-            for i in range(1, len(self.string)):
-                if self.string[-i] == ".":
-                    pos = i
-                    break
-            self.file = self.string[:len(self.string)-pos]
-            self.extention = self.string[-pos:]
-        else:
-            self.file = self.string
+#         if '.' in self.string:
+#             for i in range(1, len(self.string)):
+#                 if self.string[-i] == ".":
+#                     pos = i
+#                     break
+#             self.file = self.string[:len(self.string)-pos]
+#             self.extention = self.string[-pos:]
+#         else:
+#             self.file = self.string
 
-    def setFileName(self, name):
-        """Sets a new file name for the rename function."""
-        self.file = name
+#     def setFileName(self, name):
+#         """Sets a new file name for the rename function."""
+#         self.file = name
 
-    def getName(self): 
-        """Returns the current full name of the file."""
-        return self.string
+#     def getName(self): 
+#         """Returns the current full name of the file."""
+#         return self.string
 
-    def setName(self, string):
-        """Sets the member string var."""
-        self.string = string
+#     def setName(self, string):
+#         """Sets the member string var."""
+#         self.string = string
 
-    def getFileName(self): 
-        """Returns the current file name that it shall be renamed to."""
-        return self.file
+#     def getFileName(self): 
+#         """Returns the current file name that it shall be renamed to."""
+#         return self.file
 
-    def getFileExt(self):
-        """Returns the current file extention."""
-        return self.extention
+#     def getFileExt(self):
+#         """Returns the current file extention."""
+#         return self.extention
 
-    def rename(self):
-        """renames the file from the current name (string) to the new one (file)."""
-        self.newName = self.file + self.extention
-        os.rename(self.string, self.newName)
-        print(self.string + "-->" + self.newName)
+#     def rename(self):
+#         """renames the file from the current name (string) to the new one (file)."""
+#         self.newName = self.file + self.extention
+#         os.rename(self.string, self.newName)
+#         print(self.string + "-->" + self.newName)
             
 
 class Renamer(object):
