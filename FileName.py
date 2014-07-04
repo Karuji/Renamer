d1f497afc49c407631eecd41904b1349659910fa
Karuji <Code@Karuji.net>
2014-07-04 20:01:52 +0200

    Refactored FileName.py

--- FileName.py
+++ FileName.py
@@ -9,6 +9,30 @@ class FileName(object):
     def __init__(self, string):
         """Allows passing the current file name when creating the object."""
         self.string = string
+        self.splitName()
+
+    def getName(self): 
+        """Returns the current full name of the file."""
+        return self.string
+
+    def setName(self, string):
+        """Sets the member string var."""
+        self.string = string
+
+    def setFileName(self, name):
+        """Sets a new file name for the rename function."""
+        self.file = name
+
+    def getFileName(self): 
+        """Returns the current file name that it shall be renamed to."""
+        return self.file
+
+    def getFileExt(self):
+        """Returns the current file extention."""
+        return self.extention
+
+    def splitName(self):
+        """Splits the name into the FileName and extention."""
         pos = 0
 
         if '.' in self.string:
@@ -20,23 +44,3 @@ class FileName(object):
             self.extention = self.string[-pos:]
         else:
             self.file = self.string
-
-    def setFileName(self, name):
-        """Sets a new file name for the rename function."""
-        self.file = name
-
-    def getName(self): 
-        """Returns the current full name of the file."""
-        return self.string
-
-    def setName(self, string):
-        """Sets the member string var."""
-        self.string = string
-
-    def getFileName(self): 
-        """Returns the current file name that it shall be renamed to."""
-        return self.file
-
-    def getFileExt(self):
-        """Returns the current file extention."""
-        return self.extention
