bf530002d2ad7c2992d24d2330636d5aa6172b12
Karuji <Code@Karuji.net>
2014-09-03 00:04:08 +0200

    Updated license to MIT

--- Renamer.py
+++ Renamer.py
@@ -184,24 +184,6 @@ class Renamer(object):
             for item in self.mainList:
                 print(str(i).zfill(self.fill) + ": " + item.getFileName())
                 i +=1
-        elif cmd[1] == "w":
-            print("""This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    """)
-        elif cmd[1] == "c":
-            print("""Renamer is a program for the bulk renaming of files.
-    Copyright (C) 2013  Julian Prithard
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    """)
 
     def dir(self):
         """An attempted to replicate the command line dir function for navigation."""
@@ -328,10 +310,27 @@ class Renamer(object):
         
 def main():
     renamer = Renamer()
-    print("""Renamer  Copyright (C) 2013  Julian Pritchard
-    This program comes with ABSOLUTELY NO WARRANTY; for details type 'show w'.
-    This is free software, and you are welcome to redistribute it
-    under certain conditions; type 'show c' for details.""")
+    print("""The MIT License (MIT)
+
+Copyright (c) 2014 Julian Pritchard
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.""")
     print()
     print("Type help functions for a list of commands")
     while True:
